c8d7f891be5e81b554aeada8eec97321411315e5
Agastya Dubey <agastyas11@gmail.com>
2025-11-15 20:48:29 +1100

    finished the encoding bit

---
+++ atbash-cipher.ts
@@ -0,0 +1,58 @@
+const atbashCipher: {[key: string]: string} = {
+  'a' : 'z',
+  'b' : 'y',
+  'c' : 'x',
+  'd' : 'w',
+  'e' : 'v',
+  'f' : 'u',
+  'g' : 't',
+  'h' : 's',
+  'i' : 'r',
+  'j' : 'q',
+  'k' : 'p',
+  'l' : 'o',
+  'm' : 'n',
+  'n' : 'm',
+  'o' : 'l',
+  'p' : 'k',
+  'q' : 'j',
+  'r' : 'i',
+  's' : 'h',
+  't' : 'g',
+  'u' : 'f',
+  'v' : 'e',
+  'w' : 'd',
+  'x' : 'c',
+  'y' : 'b',
+  'z' : 'a'
+}
+
+export function encode(plainText: string): string {
+  let encrypted = "" // encrypting first
+  for (const char of plainText) {
+    if (char in atbashCipher) {
+      encrypted += atbashCipher[char]
+    } else {
+      encrypted += char
+    }
+  }
+
+  let ordered = "" // then ordering (in groups of 5)
+  let count = 0 
+  for (const char of (encrypted.replace(/\s+/g, ""))) {
+    ordered += char
+    count++
+
+    if(count % 5 === 0) {
+      ordered += " "
+    }
+  }
+
+  return (ordered.toLowerCase()).trim()
+}
+
+export function decode(cipherText: string): string {
+  throw new Error('Remove this line and implement the function')
+}
+
+console.log(encode("I love money i want 2000 dollar"))
